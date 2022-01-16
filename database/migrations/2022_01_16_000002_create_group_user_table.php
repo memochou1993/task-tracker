0ed3ca0bbf4606519f8c37d4aff220158ee37d7d
Memo Chou <memochou1993@gmail.com>
2022-01-16 22:46:54 +0800

    Add models

---
+++ database/migrations/2022_01_16_000002_create_group_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Group;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Group::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_user');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        /** @var User $user */
+        $user = User::factory()->create([
+            "name" => "root",
+            "email" => "root@email.com",
+        ]);
+
+        /** @var Collection $tasks */
+        $tasks = $user->tasks()->saveMany(Task::factory()->count(3)->make());
+
+        $tasks->each(function ($task) use ($user) {
+            $subtasks = Task::factory()->count(3)->make([
+                'user_id' => $user->id,
+                'parent_id' => $task->id,
+            ]);
+
+            $task->tasks()->saveMany($subtasks);
+        });
     }
 }
